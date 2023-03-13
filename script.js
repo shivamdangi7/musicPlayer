cdd900e2d9095b25a43d9df970b9b19ec8d32f94
shivamdangi7 <shivamdangi0111@gmail.com>
2023-03-14 01:14:39 +0530

    card-content introduced

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const jsmediatags = window.jsmediatags;
+let musicnum = 0;
 document.querySelector("#input").addEventListener("change", (event) => {
-  const file = event.target.files[0];
+  const file = event.target.files[musicnum];
 
   jsmediatags.read(file, {
     onSuccess: function (tag) {
-      // Array buffer to base64
       const data = tag.tags.picture.data;
       const format = tag.tags.picture.format;
       let base64String = "";
       for (let i = 0; i < data.length; i++) {
         base64String += String.fromCharCode(data[i]);
       }
-      // Output media tags
       document.querySelector(
         "#cover"
       ).style.backgroundImage = `url(data:${format};base64,${window.btoa(
@@ -28,3 +27,61 @@ document.querySelector("#input").addEventListener("change", (event) => {
     },
   });
 });
+let progress = document.getElementById("progress");
+let ctrlIcon = document.getElementById("ctrlIcon");
+let song = document.getElementById("song");
+
+const input = document.getElementById("input");
+// input.addEventListener("change", function () {
+//   const selectedFile = input.files[0];
+//   const fileReader = new FileReader();
+
+//   fileReader.onload = function () {
+//     song.src = fileReader.result;
+//   };
+//   fileReader.readAsDataURL(selectedFile);
+// });
+
+input.addEventListener("change", function () {
+  const selectedFile = input.files[0];
+  const fileReader = new FileReader();
+  fileReader.onload = function () {
+    song.src = fileReader.result;
+    song.play();
+  };
+  fileReader.readAsDataURL(selectedFile);
+});
+function nextFile() {
+  const currentIndex = input.selectedIndex;
+  const nextIndex = currentIndex + 1;
+  if (nextIndex < input.options.length) {
+    input.selectedIndex = nextIndex;
+    input.dispatchEvent(new Event("change"));
+  }
+}
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+
+function playPause() {
+  if (ctrlIcon.textContent === "pause") {
+    song.pause();
+    ctrlIcon.textContent = "play_arrow";
+  } else {
+    song.play();
+    ctrlIcon.textContent = "pause";
+  }
+}
+
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+};
+
+playNext();
