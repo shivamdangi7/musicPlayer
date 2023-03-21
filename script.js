aaadc0393640564e4926ad323c1a817766c5a20f
shivamdangi7 <shivamdangi0111@gmail.com>
2023-03-21 19:35:18 +0530

    next and Previous working with thumbnailchange

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 const jsmediatags = window.jsmediatags;
-let musicnum = 1;
-document.querySelector("#input").addEventListener("change", (event) => {
-  const file = event.target.files[musicnum];
+let musicnum = 0;
+function thumbnail() {
+  document.querySelector("#input").addEventListener("change", (event) => {
+    const file = event.target.files[musicnum];
+    jsmediatags.read(file, {
+      onSuccess: function (tag) {
+        console.log(tag);
+        const data = tag.tags.picture.data;
+        const format = tag.tags.picture.format;
+        let base64String = "";
+        for (let i = 0; i < data.length; i++) {
+          base64String += String.fromCharCode(data[i]);
+        }
+        document.querySelector(
+          "#cover"
+        ).style.backgroundImage = `url(data:${format};base64,${window.btoa(
+          base64String
+        )})`;
 
-  jsmediatags.read(file, {
-    onSuccess: function (tag) {
-      const data = tag.tags.picture.data;
-      const format = tag.tags.picture.format;
-      let base64String = "";
-      for (let i = 0; i < data.length; i++) {
-        base64String += String.fromCharCode(data[i]);
-      }
-      document.querySelector(
-        "#cover"
-      ).style.backgroundImage = `url(data:${format};base64,${window.btoa(
-        base64String
-      )})`;
-
-      document.querySelector("#title").textContent = tag.tags.title;
-      document.querySelector("#artist").textContent = tag.tags.artist;
-      document.querySelector("#album").textContent = tag.tags.album;
-      document.querySelector("#genre").textContent = tag.tags.genre;
-    },
-    onError: function (error) {
-      console.log(error);
-    },
+        document.querySelector("#title").textContent = tag.tags.title;
+        document.querySelector("#artist").textContent = tag.tags.artist;
+        // document.querySelector("#album").textContent = tag.tags.album;
+        // document.querySelector("#genre").textContent = tag.tags.genre;
+      },
+      onError: function (error) {
+        console.log(error);
+      },
+    });
   });
-});
+}
+
 let progress = document.getElementById("progress");
 let ctrlIcon = document.getElementById("ctrlIcon");
 let song = document.getElementById("song");
 
 const input = document.getElementById("input");
-// input.addEventListener("change", function () {
-//   const selectedFile = input.files[0];
-//   const fileReader = new FileReader();
-
-//   fileReader.onload = function () {
-//     song.src = fileReader.result;
-//   };
-//   fileReader.readAsDataURL(selectedFile);
-// });
 
 input.addEventListener("change", function () {
   const selectedFile = input.files[musicnum];
@@ -51,14 +45,6 @@ input.addEventListener("change", function () {
   };
   fileReader.readAsDataURL(selectedFile);
 });
-// function nextFile() {
-//   const currentIndex = input.selectedIndex;
-//   const nextIndex = currentIndex + 1;
-//   if (nextIndex < input.options.length) {
-//     input.selectedIndex = nextIndex;
-//     input.dispatchEvent(new Event("change"));
-//   }
-// }
 song.onloadedmetadata = function () {
   progress.max = song.duration;
   progress.value = song.currentTime;
@@ -83,7 +69,6 @@ progress.onchange = function () {
   song.play();
   song.currentTime = progress.value;
 };
-
 function playNext() {
   musicnum++;
   const selectedFile = input.files[musicnum];
@@ -93,6 +78,7 @@ function playNext() {
     song.play();
   };
   fileReader.readAsDataURL(selectedFile);
+  console.log(musicnum);
 }
 function playPrevious() {
   musicnum--;
@@ -103,4 +89,67 @@ function playPrevious() {
     song.play();
   };
   fileReader.readAsDataURL(selectedFile);
+  console.log(musicnum);
 }
+
+const skipPrevious = document.querySelector("#skipPrevious");
+skipPrevious.addEventListener("click", playPrevious);
+skipPrevious.addEventListener("click", function () {
+  // musicnum--;
+  console.log("previous file");
+
+  const file = input.files[musicnum];
+  jsmediatags.read(file, {
+    onSuccess: function (tag) {
+      console.log(tag);
+      const data = tag.tags.picture.data;
+      const format = tag.tags.picture.format;
+      let base64String = "";
+      for (let i = 0; i < data.length; i++) {
+        base64String += String.fromCharCode(data[i]);
+      }
+      document.querySelector(
+        "#cover"
+      ).style.backgroundImage = `url(data:${format};base64,${window.btoa(
+        base64String
+      )})`;
+
+      document.querySelector("#title").textContent = tag.tags.title;
+      document.querySelector("#artist").textContent = tag.tags.artist;
+    },
+    onError: function (error) {
+      console.log(error);
+    },
+  });
+});
+
+const skipNext = document.querySelector("#skipNext");
+skipNext.addEventListener("click", playNext);
+skipNext.addEventListener("click", function () {
+  // musicnum++;
+
+  const file = input.files[musicnum];
+  console.log("next file");
+  jsmediatags.read(file, {
+    onSuccess: function (tag) {
+      console.log(tag);
+      const data = tag.tags.picture.data;
+      const format = tag.tags.picture.format;
+      let base64String = "";
+      for (let i = 0; i < data.length; i++) {
+        base64String += String.fromCharCode(data[i]);
+      }
+      document.querySelector(
+        "#cover"
+      ).style.backgroundImage = `url(data:${format};base64,${window.btoa(
+        base64String
+      )})`;
+
+      document.querySelector("#title").textContent = tag.tags.title;
+      document.querySelector("#artist").textContent = tag.tags.artist;
+    },
+    onError: function (error) {
+      console.log(error);
+    },
+  });
+});
